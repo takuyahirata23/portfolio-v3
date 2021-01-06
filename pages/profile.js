13c24f2e5da57dbf0d1003c7a4f3cdbeb4fc468b
Takuya Hirata <takuyahirata4@gmail.com>
2021-01-06 07:28:19 -0500

    feat(wip): Profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,20 @@
+import { Grid, Typography } from '@material-ui/core'
 import { Layout } from '../src/elements'
 
 export default function Profile() {
   return (
     <Layout withHeader>
-      <p>profile</p>
+      <Grid container>
+        <Grid item xl={6} lg={6}>
+          <Typography variant="h2">Takuya Hirata</Typography>
+        </Grid>
+        <Grid item xl={6} lg={6}>
+          <Typography variant="body1">
+            Software engineer, working at 3merge, and author of "fp-utils-types"
+            package.
+          </Typography>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
