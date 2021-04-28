a4128bcc65b6e6efc89ea6aebb5476074624a177
Takuya Hirata <takuyahirata4@gmail.com>
2021-04-28 17:53:55 -0400

    config: test

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -5,7 +5,7 @@ function AllTheProviders({ children }: { children: React.ReactNode }) {
   return <ThemeProvider>{children}</ThemeProvider>
 }
 
-const customRender = (ui: React.ReactElement, options?: Omit<any, 'queries'>) =>
+const customRender = (ui: any, options?: any) =>
   render(ui, { wrapper: AllTheProviders, ...options })
 
 export * from '@testing-library/react'
